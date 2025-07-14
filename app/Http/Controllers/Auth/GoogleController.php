245e8b93ffd6bb22e0061d86a97dfd74c3e7d6e7
hatim47 <hatimnoor47@gmail.com>
2025-07-14 18:24:15 +0500

    hatim

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\UserTeam;
+use App\Models\Team;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
@@ -24,7 +26,6 @@ class GoogleController extends Controller
         try {
             $googleUser = Socialite::driver('google')->user();
             // dd($googleUser);
-
             // Find or create user
             $user = User::updateOrCreate(
                 ['google_id' => $googleUser->id],
@@ -34,7 +35,17 @@ class GoogleController extends Controller
                     'password' => bcrypt(Str::random(12)), // Generate a random password
                 ]
             );
+   $defaultTeam = Team::create([
+            'name' => $googleUser->name,
+            // other default fields if required
+        ]);
 
+        // Add user to this new team as Owner
+        UserTeam::create([
+            'team_id' => $defaultTeam->id,
+            'user_id' => $user->id,
+            'status' => 'Owner',
+        ]);
             // Log in the user
             Auth::login($user);
 session(['user_id' => Auth::id()]);
